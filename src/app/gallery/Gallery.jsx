c277317027dd7ce88180efd4f5afea21ffa0d759
mahshid <mahshidgholipour@gmail.com>
2025-10-16 14:08:12 +0100

    edit photo page

--- src/app/gallery/Gallery.jsx
+++ src/app/gallery/Gallery.jsx
@@ -1,17 +1,32 @@
-"use client";
-import { useState } from "react";
 import Image from "next/image";
 
 const projects = [
-  { src: "/images/1.jpg", title: "Project One", category: "Residential" },
-  { src: "/images/2.jpg", title: "Project Two", category: "Commercial" },
-  { src: "/images/3.jpg", title: "Project Three", category: "Industrial" },
-  { src: "/images/5.jpg", title: "Project Five", category: "Industrial" },
-  { src: "/images/6.jpg", title: "Project Six", category: "Industrial" },
-  { src: "/images/7.jpg", title: "Project Seven", category: "Industrial" },
-  { src: "/images/8.jpg", title: "Project Eight", category: "Industrial" },
-  { src: "/images/9.jpg", title: "Project Nine", category: "Industrial" },
-  { src: "/images/10.jpg", title: "Project Ten", category: "Industrial" },
+  { src: "/images/1.jpg"},
+  { src: "/images/2.jpg"},
+  { src: "/images/3.jpg"},
+  { src: "/images/5.jpg"},
+  { src: "/images/6.jpg"},
+  { src: "/images/7.jpg"},
+  { src: "/images/8.jpg"},
+  { src: "/images/9.jpg"},
+  { src: "/images/10.jpg"},
+  { src: "/images/11.jpg"},
+  { src: "/images/12.jpg"},
+  { src: "/images/13.jpg"},
+  { src: "/images/14.jpg"},
+  { src: "/images/15.jpg"},
+  { src: "/images/16.jpg"},
+  { src: "/images/17.jpg"},
+  { src: "/images/18.jpg"},
+  { src: "/images/19.jpg"},
+  { src: "/images/20.jpg"},
+  { src: "/images/21.jpg"},
+  { src: "/images/22.jpg"},
+  { src: "/images/23.jpg"},
+  { src: "/images/24.jpg"},
+  { src: "/images/25.jpg"},
+  { src: "/images/26.jpg"},
+
 ];
 
 export default function Gallery() {
